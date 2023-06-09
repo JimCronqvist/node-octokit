f6fda9607a08aa3fdeeb4f9f32f77f2a352cdcaf
Jim Cronqvist <jim.cronqvist@gmail.com>
2023-06-09 21:59:01 +0200

    Initial commit

--- playground.js
+++ playground.js
@@ -4,15 +4,17 @@ const { paginateRest } = require('@octokit/plugin-paginate-rest');
 const { restEndpointMethods } = require('@octokit/plugin-rest-endpoint-methods');
 const { throttling } = require("@octokit/plugin-throttling");
 
-const MyOctokit = Octokit.plugin(paginateRest, restEndpointMethods, throttling);
+const MyOctokit = Octokit.plugin(paginateRest, restEndpointMethods); // , throttling
 const octokit = new MyOctokit({ auth: process.env.GITHUB_TOKEN });
 
 void async function main() {
 
-  const repos = await octokit.paginate('GET /search/repositories', {
+  let repos = await octokit.paginate('GET /search/repositories', {
     q: 'archived:false+org:octokit',
+    //sort: 'updated',
     per_page: 100,
   });
+  repos = repos.map(repo => _.pick(repo, ['name', 'full_name', 'language', 'topics', 'visibility']));
   console.log(repos);
 
 }();
